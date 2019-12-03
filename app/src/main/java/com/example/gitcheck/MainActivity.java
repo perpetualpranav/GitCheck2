4d45eaaab49bfa77304843e48281f0093a1843c1
Pranav <pranavunnikrishnan@gmail.com>
2019-12-03 19:19:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gitcheck/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gitcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testupload(){
+        int i= 1 + 1;
+    }
+
+}
